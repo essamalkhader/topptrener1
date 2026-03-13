c42ebe900b941fb5e2d56095b6102490b3ff797b
Esam Al-Baadani <essamalkhadeer@gmail.com>
2026-03-13 11:02:08 +0100

    Add public home page and session list view with templates

--- training/views.py
+++ training/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Session
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+def session_list(request):
+    sessions = Session.objects.all().order_by("start_datetime")
+    return render(request, "training/session_list.html", {"sessions": sessions})
